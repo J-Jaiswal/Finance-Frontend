e4af9d788e34c7d34123120c53d5de755e99dd02
Jayesh <jayeshjaiswal2510@gmail.com>
2024-09-11 20:06:56 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="flex justify-center text-[26px] text-[#61df58] w-full">
+        App working good!
+      </h1>
+    </>
+  );
+}
+
+export default App;
